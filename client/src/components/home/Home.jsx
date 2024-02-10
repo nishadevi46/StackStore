ff3e5569607ec3c56b338ffef0b408e23e57e527
nishadevi46 <nishadevi4628@gmail.com>
2024-02-10 19:56:24 +0530

    banner added

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,8 +1,17 @@
 import NavBar from './NavBar'
+import Banner from './Banner';
+import { Box, styled } from '@mui/material';
+const Component = styled(Box)`
+padding:10px 10px;
+background:#0F0F0F;
+`
 const Home = ()=>{
 return(
     <>
   <NavBar/>
+  <Component>
+    <Banner/>
+  </Component>
     </>
 )
 }
