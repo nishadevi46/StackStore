f957d5ed2b68289225037a574eee478bbbb49c2e
nishadevi46 <nishadevi4628@gmail.com>
2024-02-11 17:58:59 +0530

    signup

--- client/src/components/header/CustomButton.jsx
+++ client/src/components/header/CustomButton.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { Box, Button, Typography, styled} from "@mui/material"
 import {ShoppingCart} from '@mui/icons-material';
+
+import LoginDialog from "../login/LogibDialog";
+
 const Wrapper = styled(Box)`
 display:flex;
 margin:0 3% 0 auto;
@@ -23,15 +27,22 @@ font-weight:600;
 height:32px;
 `
 const CustomButton =()=>{
+    const [open, setOpen] = useState(false);
+
+   const openDialog = ()=>{
+        setOpen(true)
+    }
+
     return (
         <Wrapper>
-            <LoginButton variant="contained">login</LoginButton>
+            <LoginButton variant="contained" onClick={()=>{openDialog()}}>login</LoginButton>
             <Typography style={{marginTop:3, width:135 }}>become a seller</Typography>
             <Typography style={{marginTop:3}}>more</Typography>
             <Container>
             <ShoppingCart/>
              <Typography>cart</Typography>
             </Container>
+            <LoginDialog open={open} setOpen={setOpen}/>
         </Wrapper>
     )
 }
---
+++ client/src/components/login/LogibDialog.jsx
@@ -0,0 +1,114 @@
+import {Dialog, Box, TextField, Typography, Button, styled} from '@mui/material'
+import { useState } from 'react'
+
+const Componenet = styled(Box)`
+  height:80vh;
+  width:60vh;
+  display:flex;
+  background:#B2A59B;
+`
+const Wrapper = styled(Box)`
+display:flex;
+flex-direction:column;
+padding:25px 35px;
+flex:1;
+& >div, & > button, & >p{
+    margin-top:20px;
+}
+`
+const LoginButton = styled(Button)`
+ text-transform:none;
+ background:#304D30;
+ color:#fff;
+ height:48px;
+ border-radius:2px;
+
+`
+const RequestOtp = styled(Button)`
+ text-transform:none;
+ background:#EEF0E5;
+ color:#304D30;
+ height:48px;
+ border-radius:2px;
+ box-shadow: 0 2px 4px 0 rgb(0 0 0/20%)
+`
+const Text = styled(Typography)`
+  font-size:16px;
+  color:#163020;
+`
+const CreateAccount = styled(Typography)`
+font-size:14px;
+text-align:center;
+color:#163020;
+font-weight:600;
+cursor:pointer;
+`
+const accountInitialValues={
+    login:{
+        view:'login'
+    },
+    signup:{
+      view:'sign-up'
+    }
+}
+
+const signupIntialValue = {
+    firstname:'',
+    lastname:'',
+    username:'',
+    email:'',
+    password:'',
+    phone:''
+}
+const LoginDialog = ({open, setOpen})=>{
+ 
+    const [account, toggleAccount]=useState(accountInitialValues.login)
+    const [signup, setSignup] = useState(signupIntialValue)
+
+    const handleClose = ()=>{
+        setOpen(false)
+        toggleAccount(accountInitialValues.login)
+    }
+    const toggleSignup = ()=>{
+        toggleAccount(accountInitialValues.signup)
+    }    
+    const onInputchange = (e)=>{
+        setSignup({...signup, [e.target.name]:e.target.value})
+    }
+    const signupUser=()=>{
+        
+    }
+    return(<>
+        <Dialog open={open} onClose={handleClose} PaperProps={{sx:{maxwidth:'unset'}}}>
+         <Componenet>
+        { account.view ==='login'? <Wrapper>
+        <Typography>Welcome to access account</Typography>
+            <TextField variant='standard' label='Enter Email/Mobile number'/>
+            <TextField variant='standard' label='Enter Password'/>
+            <Text>By continuing, I agree to the Terms of Use & Privacy Policy</Text>
+            <LoginButton>
+            Login
+            </LoginButton>
+            <Typography style={{textAlign:'center'}}>OR</Typography>
+            <RequestOtp>Request OTP</RequestOtp>
+            <CreateAccount onClick={()=>toggleSignup()}>New to StackStore? Create your account</CreateAccount>
+        </Wrapper>
+        :
+        <Wrapper>
+        <Typography>Welcome to access account</Typography>
+            <TextField variant='standard' label='Enter FirstName' name='firstname' onChange={(e)=>onInputchange(e)}/>
+            <TextField variant='standard' label='Enter LastNmae'  name='lastname'onChange={(e)=>onInputchange(e)}/>
+            <TextField variant='standard' label='Enter UserName' name='username' onChange={(e)=>onInputchange(e)}/>
+            <TextField variant='standard' label='Enter Email' 
+            name='email'onChange={(e)=>onInputchange(e)}/>
+            <TextField variant='standard' label='Enter Password' name='password' onChange={(e)=>onInputchange(e)}/>
+            <TextField variant='standard' label='Enter Phone' 
+            name='phone'onChange={(e)=>onInputchange(e)}/>
+            <LoginButton onClick={()=>signupUser()}>Continue</LoginButton>
+        </Wrapper>
+        }
+         </Componenet>
+        </Dialog>
+    </>)
+}
+export default LoginDialog;
