5252b7ac087e01a4bb48eb977cc4c15e3fa9076e
nishadevi46 <nishadevi4628@gmail.com>
2024-02-13 23:42:42 +0530

    products

--- controller/product-controller.js
+++ controller/product-controller.js
@@ -4,6 +4,6 @@ export const getProducts = async(request,response)=>{
        const products = await Product.find({});
        response.status(200).json(products);
        } catch (error) {
-        
+        response.status(500).json({message:error.message})
        }
 }
