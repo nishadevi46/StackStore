faf8af79324f77b630ff06a604fa06021371f1fa
nishadevi46 <nishadevi4628@gmail.com>
2024-02-14 02:39:50 +0530

    redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,7 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
+import {thunk} from 'redux-thunk'
+import { getProductsReducer } from './reducers/productReducer';
 const reducer = combineReducers({
     getProducts:getProductsReducer
 })
