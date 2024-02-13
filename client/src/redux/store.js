eb496a3e9b74d7ff3a9f7589875cc8b9673b3728
nishadevi46 <nishadevi4628@gmail.com>
2024-02-14 01:09:18 +0530

    middleware

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,13 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 const reducer = combineReducers({
     getProducts:getProductsReducer
 })
+const middleware = [thunk]
+
 const store = createStore(
   reducer,
-  composeWithDevTools()
+  composeWithDevTools(applyMiddleware(...middleware))
 )
+export default store;
