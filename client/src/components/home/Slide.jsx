d56fe694133a1b898298658384400da885b39a6f
nishadevi46 <nishadevi4628@gmail.com>
2024-02-15 02:40:51 +0530

    product slider

--- client/src/components/home/Slide.jsx
+++ client/src/components/home/Slide.jsx
@@ -1,4 +1,7 @@
 import Carousel from 'react-multi-carousel'
+import "react-multi-carousel/lib/styles.css"
+import Countdown from 'react-countdown'
+import { Box, Typography, Button, Divider, styled} from '@mui/material';
 const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -13,14 +16,81 @@ const responsive = {
       items: 1
     }
   };
+  const Image = styled('img')({
+    width:'auto',
+    height:150,
+
+  })
+  const Componenet = styled(Box)`
+  margin-top:10px;
+background-color:#191717;
+  `
+  const Deal = styled(Box)`
+  padding:15px 20px;
+    display:flex;
+    color:#fff;
+  `
+  const Timer = styled(Box)`
+  display:flex;
+  margin-left:10px;
+  align-items:center;
+  color:#FAF0E6;
+  `
+  const DealText = styled(Typography)`
+  font-size:22px;
+  font-weight:600;
+  margin-right:25px;
+  line-height:32px;
+
+  `
+  const Text = styled(Typography)`
+  font-size:14px;
+  color:#F9F5F6;
+  margin-top:5px;
+  `
+  const ViewButton = styled(Button)`
+  margin-left:auto;
+  background-color:#50577A;
+  border-radius:2px;
+  font-size:13px;
+  font-weight:600;
+  `
 const Slide = ({products})=>{
+    const timerURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/timer_a73398.svg';
+    const renderer = ({ hours, minutes, seconds}) => {
+        return <Box variant='span'>{hours} : {minutes} : {seconds}Left</Box>
+      };
+      
     return (
+        <Componenet>
+        <Deal><DealText>Deal of the Day </DealText>
+        <Timer> <img src={timerURL} alt="timer" style={{width:24}}/>
+        <Countdown date={Date.now() + 5.04e+7} renderer={renderer} /></Timer>
+        <ViewButton variant='contained' color='primary'>View all</ViewButton>
+        </Deal>
+        <Divider style={{ backgroundColor: '#fff' }}/>
         <Carousel
-        responsive={responsive}>
+        responsive={responsive}
+        swipeable={false}
+  draggable={false}
+  autoPlay={ true }
+  infinite={true}
+  autoPlaySpeed={3000}
+  keyBoardControl={true}
+  centerMode={true}
+  dotListClass="custom-dot-list-style"
+     itemClass="carousel-item-padding-40-px"
+     containerClass="carousel-container">
        { products.map(product => (
-            <img src={product.url} alt="product" />
+        <Box textAlign="center" style={{padding:'25px 15px'}}>
+            <Image src={product.url} alt="product" />
+            <Text style={{fontWeight:'bold'}}>{product.title.shortTitle}</Text>
+            <Text style={{color:'#14C38E'}}>{product.discount}</Text>
+            <Text style={{color:'#DFD3C3',opacity:'.6'}}>{product.tagline}</Text>
+            </Box>
         ))}
         </Carousel>
+        </Componenet>
     )
 }
 export default Slide
