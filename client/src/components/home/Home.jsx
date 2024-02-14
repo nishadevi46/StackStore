29c2ae07e53b27e05d55851663ce583aee94cede
nishadevi46 <nishadevi4628@gmail.com>
2024-02-14 23:41:28 +0530

    products

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,21 @@
-import NavBar from './NavBar'
+import { useEffect } from 'react';
+import NavBar from './NavBar';
 import Banner from './Banner';
 import { Box, styled } from '@mui/material';
+import { getProducts } from '../../redux/actions/productActions';
+import {useDispatch, useSelector} from 'react-redux'
 const Component = styled(Box)`
 padding:10px 10px;
 background:#0F0F0F;
 `
 const Home = ()=>{
+  const {products}=useSelector(state=>state.getProducts)
+  console.log(products)
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getProducts())
+  }, [dispatch]
+  )
 return(
     <>
   <NavBar/>
