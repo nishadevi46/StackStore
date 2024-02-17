cf5a11fb012267dc658469ff8afb4cc4874fd288
nishadevi46 <nishadevi4628@gmail.com>
2024-02-17 09:20:14 +0530

    working with detailProduct page

--- client/src/components/details/DetailView.jsx
+++ client/src/components/details/DetailView.jsx
@@ -1,4 +1,13 @@
+  import { useEffect } from "react"
+  import {useDispatch}  from 'react-redux'
+  import {useParams} from 'react-router-dom'
+  import { getproductDetails } from "../../redux/actions/productActions"
 const DetailView = ()=>{
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    useEffect(()=>{
+     dispatch(getproductDetails(id));
+    },[dispatch, id])
     return (
         <>
             hello
