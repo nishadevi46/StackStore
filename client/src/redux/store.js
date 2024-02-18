56ae0d0e075b38deb46e3a52baa626ddbbb33bb5
nishadevi46 <nishadevi4628@gmail.com>
2024-02-18 12:36:37 +0530

    cart

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,9 +2,11 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {thunk} from 'redux-thunk'
 import { getProductReducer, getProductDetailsReducer } from './reducers/productReducer';
+import {cartReducer} from './reducers/cartReducer'
 const reducer = combineReducers({
     getProducts:getProductReducer,
-    getProductDetails:getProductDetailsReducer
+    getProductDetails:getProductDetailsReducer,
+    cart:cartReducer
 })
 const middleware = [thunk]
 
