3d4ba243e85c609644fafc54c985fe78d69e0b75
nishadevi46 <nishadevi4628@gmail.com>
2024-02-27 01:46:00 +0530

    deployvariable

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -2,7 +2,7 @@
 import * as actionTypes from '../constants/productConstant'
 
 import axios from 'axios'
-const URL = 'http://localhost:8000'
+const URL = ''
 
 
 export const getProducts = ()=>async (dispatch)=>{
--- index.js
+++ index.js
@@ -5,13 +5,20 @@ import bodyParser from 'body-parser';
 import Connection from './database/db.js';
 import DefaultData from './default.js';
 import Router from './router/route.js'
-
+import path from 'path'
+const __dirname = path.resolve();
 const app =express()
 dotenv.config()
 app.use(bodyParser.json({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 app.use('/',Router);
+app.use(express.static(path.join(__dirname, "./client/build")))
+app.get('*', function(_, res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"), function(error){
+        res.status(500).send(err);
+    })
+})
 const PORT = 8000||process.env.port;
 const USERNAME=process.env.DB_USERNAME
 const PASSWORD=process.env.DB_PASSWORD
